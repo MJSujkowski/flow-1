025cfa382042e4076661bd9ca347e7f5dd617086
MichaelGSanchez <michaelgregorysanchez@gmail.com>
2019-01-24 16:50:39 -0700

    added output directory to gitignore

--- src/edu/cnm/deepdive/GuessANumber.java
+++ src/edu/cnm/deepdive/GuessANumber.java
@@ -1,39 +1,53 @@
 package edu.cnm.deepdive;
 
-
 import java.util.Random;
 import java.util.Scanner;
 
 public class GuessANumber {
-    private static Random random = new Random();
-    private static Scanner input = new Scanner(System.in);
+  private static Random random = new Random();
+  private static Scanner input = new Scanner(System.in);
+  public static void main(String[] args) {
+    int secret;
+    int guess;
+    String userSaysYes;
+    do {
+      userSaysYes = playOneRound();
+    } while (userSaysYes.toLowerCase().charAt(0) == 'y');
+  }
 
-    public static void main(String[] args) {
-        int secret;
-        int guess;
-        String userSaysYes;
+  private static String playOneRound() {
+    int secret;
+    int guess;
+    String userSaysYes;
+    secret = random.nextInt(100) + 1;
+    System.out.println(
+        "Please enter a number between 1 and 100. Or enter 0 to quit.");
+    do {
+      guess = getGuess(secret);
+    } while (guess != secret);
+    System.out.println("Would you like to play again?");
+    do {
 
-        do {
-            secret = random.nextInt(100) + 1;
-            System.out.println("Please enter a number between 1 and 100. Or enter 0 to quit.");
+      userSaysYes = input.nextLine().trim().toLowerCase();
+    } while (userSaysYes.isEmpty());
+    return userSaysYes;
+  }
 
-            do {
-                guess = input.nextInt();
-                if (guess == 0) {
-                    break;
-            }
-                if (guess > secret) {
-                    System.out.println("Your guess is too high!");
-                }
-                if (guess < secret) {
-                    System.out.println("Your guess is too low.");
-                }
-            } while (guess != secret);
-            System.out.println("Your guess was correct!");
-            System.out.println("Would you like to play again?");
-            input.nextLine();
-            userSaysYes = input.nextLine();
-        } while (userSaysYes.toLowerCase().charAt(0) == 'y');
-
+  private static int getGuess(int secret) {
+    int guess;
+    guess = input.nextInt();
+    if (guess == 0) {
+      System.out.println("See you later.");
     }
+    else if (guess > secret) {
+      System.out.println("Your guess is too high!");
+    }
+    else if (guess < secret) {
+      System.out.println("Your guess is too low.");
+    } else {
+      System.out.println("Your guess was correct!");
+    }
+    return guess;
+  }
 }
+
